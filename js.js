f034638e33dd138c4fb84f4e0bf29a2462479758
dmitriy.lipgart <dmitriy.lipgart@gmail.com>
2018-04-14 07:43:27 +0300

    adaptive changes

---
+++ js.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  checkMedia();
+
+$(window).on('resize',  
+
+
+        function() {
+
+          checkMedia();
+        }
+
+
+
+
+
+  );
+
+});
+
+
+
+
+
+  function checkMedia() {
+
+   
+if (window.matchMedia('(max-width: 600px)').matches){
+
+    $('.reviews').slick ({
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+          });
+
+} else {
+
+$('.reviews').slick ({
+                    infinite: true,
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+          });
+
+
+}
+
+   }
+
